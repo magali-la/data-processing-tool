02bc525d0f11847a6ffb8fa62d9136b443be1f63
Mag <166319982+magali-la@users.noreply.github.com>
2025-11-07 13:24:47 -0500

    (task 4): refactored total cost function with discount conditional and test variable

--- calculateTotalCost.js
+++ calculateTotalCost.js
@@ -1,17 +1,23 @@
-function calculateTotalCost(price, quantity, taxRate){
+function calculateTotalCost(price, quantity, taxRate, discount){
     // handle edge case - inputs are not numbers
     if (typeof(price) !== 'number' || typeof(quantity) !== 'number' || typeof(taxRate) !== 'number'){
         return 'Invalid input';
     }
 
     let totalCost = (price * quantity) * (1 + taxRate);
-    
+
+    // adjust price if sale is ongoing
+    if (discount){
+        totalCost -= 2;
+    }
+
     return totalCost;
 }
 
 // define test variables
-let itemPrice = 'free';
+let itemPrice = 10;
 let itemQuantity = 1;
 let stateTax = 0.5;
+let onSale = true;
 
-console.log(calculateTotalCost(itemPrice, itemQuantity, stateTax));
+console.log(calculateTotalCost(itemPrice, itemQuantity, stateTax, onSale));
