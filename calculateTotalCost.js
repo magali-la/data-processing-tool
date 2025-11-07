f1dd514cdf2f45f92119b46f0bd5850ae2d70c18
Mag <166319982+magali-la@users.noreply.github.com>
2025-11-07 12:57:04 -0500

    (task 2): add edge case handling for non-number input

--- calculateTotalCost.js
+++ calculateTotalCost.js
@@ -1,11 +1,16 @@
 function calculateTotalCost(price, quantity, taxRate){
+    // handle edge case - inputs are not numbers
+    if (typeof(price) !== 'number' || typeof(quantity) !== 'number' || typeof(taxRate) !== 'number'){
+        return 'Invalid input';
+    }
+
     let totalCost = (price * quantity) * (1 + taxRate);
     
     return totalCost;
 }
 
 // define test variables
-let itemPrice = 10;
+let itemPrice = 'free';
 let itemQuantity = 1;
 let stateTax = 0.5;
 
